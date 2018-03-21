7babe95873f7bd74547582157a52277a59ad3095
Mehret Kulala <merutaye@gmail.com>
2018-03-20 21:14:00 -0500

    added the function to main.js and div to the index.html[C

--- main.js
+++ main.js
@@ -54,3 +54,24 @@ const students = [
         catchPhrase: "Multi-channelled solution-oriented artificial intelligence"
     }
 ];
+
+//Print to dom function
+const printToDom = (domString, divId) => {
+    const printTo = document.getElementById(divId);
+    printTo.innerHTML = domString;
+};
+
+const buildDomString = (studentArray) => {
+    let domString = '';
+    studentArray.forEach((student) => {
+        domString +=`<div class="card">`;
+        domString += `<h1>${student.firstName} ${student.lastName}</h1>`;
+        domString +=`<h3>${student.catchPhrase}</h3>`;
+        domString += `<img src="${student.avatar}" alt="">`;
+        domString += `<button class="button">Brought Pie</button>`;
+        domString += `</div>`;
+    });
+    printToDom(domString, "card-holder");
+}
+
+buildDomString(students);
